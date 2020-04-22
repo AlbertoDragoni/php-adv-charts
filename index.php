d1d24e4b02af28f9d2ea1df5a111209b8c48d9db
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-22 16:38:13 +0200

    1 metodo

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
         $(document).ready(function(){
 
           var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
-          <?php $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322]; ?>;
-          var data = <?php echo json_encode($data); ?>;
+          <?php include 'data.php'; ?>
+          var data = <?php echo $database; ?>;
           var ctx = $('#grafico');
           var chart = new Chart(ctx, {
               type: 'line',
