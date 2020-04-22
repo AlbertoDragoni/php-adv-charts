54bd24edff819efdebf4935d80d06abc7422e274
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-22 17:01:49 +0200

    2 metodo

---
+++ index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <meta charset="utf-8">
+    <title>Metodo 2</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'data.php' ?>;
+      <canvas id="grafico" data-database="<?php echo $database; ?>"></canvas>
+      <script>
+
+      </script>
+    </div>
+   <script src="js/main.js" charset="utf-8"></script>
+  </body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  var data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
+  // var data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
+  var data = $('#grafico').data('database');
   var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
   var ctx = $('#grafico');
   var chart = new Chart(ctx, {
