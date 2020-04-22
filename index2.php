8be10d1dac93871cc3aecbe6f131a3c223c54c40
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-22 17:17:53 +0200

    3 metodo

--- index2.php
+++ index2.php
@@ -5,7 +5,7 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <meta charset="utf-8">
-    <title>Metodo 2</title>
+    <title>Metodo 2 e 3</title>
   </head>
   <body>
     <div class="container">
--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+// METODO 3
+  $.ajax({
+    url: 'server.php',
+    method: 'GET',
+    success: function(data){
+      var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+      var ctx = $('#grafico');
+      var chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: mesi,
+              datasets: [{
+                  label: 'Grafico con Php',
+                  backgroundColor: 'blue',
+                  borderColor: 'black',
+                  data: data
+              }]
+          },
+          options: {}
+      });
+    },
+    error: function(){
+      alert('ciao');
+    }
+  });
+
+
+
+
+
+
+
+
+  /*  METODI 1 E 2
   // var data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
   var data = $('#grafico').data('database');
   var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
@@ -22,6 +56,6 @@ $(document).ready(function(){
       options: {}
   });
 
-
+*/
 
 });
