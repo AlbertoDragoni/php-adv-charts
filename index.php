20953211a8ff8c21c0bf6c4ab4e4f8d43d5b43b1
Alberto Dragoni <albertodragoni@yahoo.it>
2020-04-22 15:45:35 +0200

    metodo 0,5

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
+    <div class="container">
+      <canvas id="grafico"></canvas>
+    </div>
     <script src="js/main.js" charset="utf-8"></script>
   </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
+  var mesi = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
+
+  var ctx = $('#grafico');
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+          labels: mesi,
+          datasets: [{
+              label: 'Grafico con Php',
+              backgroundColor: 'blue',
+              borderColor: 'black',
+              data: data
+          }]
+      },
+
+      // Configuration options go here
+      options: {}
+  });
+
+
+
+
+
+
+
+
+});
